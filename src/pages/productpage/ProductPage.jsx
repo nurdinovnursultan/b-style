ffb33fc9b925561a685520b3fbc9ad2216f5b802
Nurs21 <nurdinovnursultan11@gmail.com>
2022-09-16 14:09:53 +0600

    login done

---
+++ src/pages/productpage/ProductPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/header/Header';
+import SideBar from '../../components/sidebar/SideBar';
+
+const ProductPage = () => {
+    return (
+        <div>
+            <Header />
+            <SideBar />
+        </div>
+    );
+};
+
+export default ProductPage;

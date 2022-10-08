35a9e5edff4a416ce1df642db8e1ed3a706d7fd3
daurenabasov <daurenabasov987@gmail.com>
2022-10-08 14:07:33 +0600

    create branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import SideBar from "./components/sidebar/SideBar";
-import LoginPage from "./pages/loginPage/LoginPage";
+import LoginPage from "./pages/loginpage/LoginPage";
 import SemiFinishedPage from "./pages/semiFinishedPage/SemiFinishedPage";
 import ElaborationPage from "./pages/elaborationPage/ElaborationPage";
 import CatalogPage from "./pages/catalogPage/CatalogPage";
@@ -17,7 +17,7 @@ import SalesItem from "./components/salesItem/SalesItem";
 import SemiFinishedDetails from "./components/semiFinishedDetails/SemiFinishedDetails";
 
 function App() {
-  const [sidebar, setSidebar] = useState(true)
+  const [sidebar, setSidebar] = useState(true);
 
   return (
     <div className="App">
@@ -26,14 +26,23 @@ function App() {
         <SideBar status={sidebar} setSidebar={setSidebar} />
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="semifinished/*" element={<SemiFinishedPage status={sidebar} />}>
+          <Route
+            path="semifinished/*"
+            element={<SemiFinishedPage status={sidebar} />}
+          >
             <Route path=":id" element={<SemiFinishedDetails />} />
           </Route>
-          <Route path="elaboration" element={<ElaborationPage status={sidebar} />} />
+          <Route
+            path="elaboration"
+            element={<ElaborationPage status={sidebar} />}
+          />
           <Route path="catalog/*" element={<CatalogPage status={sidebar} />}>
             <Route path=":id" element={<CatalogItem />} />
           </Route>
-          <Route path="employees/*" element={<EmployeesPage status={sidebar} />}>
+          <Route
+            path="employees/*"
+            element={<EmployeesPage status={sidebar} />}
+          >
             <Route path=":id" element={<Employee />} />
           </Route>
           <Route path="stock/*" element={<StockPage status={sidebar} />}>
